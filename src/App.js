6897100b83867440a011bf08a9fdac0ab4fd2ce7
abhishek <abhisasa2001@gmail.com>
2022-09-12 16:32:59 +0530

    chatapp

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Profile from './pages/Profile'
+import PrivateRoute from "./components/PrivateRoute";
+import PrivateLog from "./components/PrivateLog";
+
+function App() {
+  return (
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+        <Route element={<PrivateRoute/>}>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/profile" element={<Profile/>}></Route>
+        </Route>
+        <Route element={<PrivateLog/>}>
+          <Route path="/login" element={<Login/>}></Route>
+          <Route path="/register" element={<Register/>}></Route>
+        </Route>
+        </Routes>
+      </BrowserRouter>
+  );
+}
+
+export default App;
